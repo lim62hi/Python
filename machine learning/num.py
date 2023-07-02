4686e3b17dee9747c15474df254ea1a89ab5d7a5
lim62 <lim62hi@gmail.com>
2023-07-02 14:11:44 +0300

    numpy

---
+++ machine learning/num.py
@@ -0,0 +1,4 @@
+import numpy as np
+mas = np.array([1, 2, 3, 4])
+mas = mas.reshape(2,2)
+print(mas[1, 1])
---
+++ machine learning/test.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+print(f'Version: {tf.__version__}')
+print(f'Version: {tf.__version__}')
