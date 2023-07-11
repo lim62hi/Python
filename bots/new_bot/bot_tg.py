0024cb09e7da5fa7c0a984e84eaa7ab92f0cb583
lim62 <lim62hi@gmail.com>
2023-07-11 19:12:26 +0300

    bot

--- bots/new_bot/bot_tg.py
+++ bots/new_bot/bot_tg.py
@@ -1,20 +1,55 @@
-import aiogram as ai
+import aiogram as aio
 import os
 
-bot = ai.Bot(token=os.getenv('TOKEN'))
-dp = ai.dispatcher.Dispatcher(bot)
+bot = aio.Bot(token=os.getenv('TOKEN'))
+dp = aio.dispatcher.Dispatcher(bot)
+HELP = '''/help - список команд
+/work - режим работы
+/location - расположение кафе
+
+Приятного аппетита!'''
+rej = 'Режим работы: любой день недели 8:00 - 20:00'
+rasp = 'Улица Колбасная 15А'
+ls = 'Пожалуйста, напишите мне в личные сообщения, чтобы я мог с Вами общаться через них и присылать Вам личную информацию!'
+
+async def start_bot(_):
+    print('Бот включился и исправно работает!')
+
+@dp.message_handler(commands=['start', 'help'])
+async def Help(message : aio.types.message):
+    try:
+        await bot.send_message(message.from_user.id, HELP)
+        await message.delete()
+    except:
+        await message.reply(f'{HELP}\n\n{ls}')
+
+@dp.message_handler(commands=['work'])
+async def rejim(message : aio.types.message):
+    try:
+        await bot.send_message(message.from_user.id, rej)
+        await message.delete()
+    except:
+        await message.reply(f'{rej}\n\n{ls}')
+
+@dp.message_handler(commands=['location'])
+async def ras(message : aio.types.message):
+    try:
+        await bot.send_message(message.from_user.id, rasp)
+        await message.delete()
+    except:
+        await message.reply(f'{rasp}\n\n{ls}')
 
 @dp.message_handler()
-async def echo(message : ai.types.message):
+async def echo(message : aio.types.message):
     if message.text.lower() == 'привет':
         await message.reply('И тебе привет!')
     elif message.text.lower() == 'как дела':
         await message.reply('Отлично, а у тебя?')
     else:
-        #await message.answer(message.text)
-        await message.reply(message.text)
+        await message.answer(message.text)
+        #await message.reply(message.text)
         #await bot.send_message(message.from_user.id, message.text)
 
 
 
-ai.utils.executor.start_polling(dp, skip_updates=True)
+aio.utils.executor.start_polling(dp, skip_updates=True, on_startup=start_bot)
