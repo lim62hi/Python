67d6d1cbbb972a2a861b9655b71a7c65656fc3f3
lim62 <lim62hi@gmail.com>
2023-07-12 21:09:24 +0300

    bot_mat

--- bots/new_bot/bot_tg.py
+++ bots/new_bot/bot_tg.py
@@ -1,5 +1,5 @@
 import aiogram as aio
-import os
+import os, json, string
 
 bot = aio.Bot(token=os.getenv('TOKEN'))
 dp = aio.dispatcher.Dispatcher(bot)
@@ -40,15 +40,11 @@ async def ras(message : aio.types.message):
         await message.reply(f'{rasp}\n\n{ls}')
 
 @dp.message_handler()
-async def echo(message : aio.types.message):
-    if message.text.lower() == 'привет':
-        await message.reply('И тебе привет!')
-    elif message.text.lower() == 'как дела':
-        await message.reply('Отлично, а у тебя?')
-    else:
-        await message.answer(message.text)
-        #await message.reply(message.text)
-        #await bot.send_message(message.from_user.id, message.text)
+async def ban_word(message : aio.types.message):
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()}\
+    .intersection(set(json.load(open('words.json')))) != set():
+        await message.reply('Маты запрещены!')
+        await message.delete()
 
 
 
