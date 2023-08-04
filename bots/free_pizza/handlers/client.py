56758041dcfc5db12caec77aaba74a608e1c60c2
lim62 <lim62hi@gmail.com>
2023-08-04 13:50:30 +0300

    bot

--- bots/free_pizza/handlers/client.py
+++ bots/free_pizza/handlers/client.py
@@ -1,14 +1,22 @@
 from aiogram import types, Dispatcher
 from create import bot
-from keyboards.client_kb import kb_client, kb_inline
-from keyboards.admin_kb import kb_admin
+from keyboards.client_kb import *
+from keyboards.admin_kb import *
 from aiogram.dispatcher.filters import Text
 from data_base.sqlite_db import read_info
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 HELP = 'Вы можете узнать расписание работы, расположение и меню\nПриятного аппетита!'
 rej = 'Пн - пт 8:00 - 20:00, сб 10:00 - 18:00'
 rasp = 'Улица Колбасная, 15А'
 
+dat = None
+pizza = None
+
+class FSM_client(StatesGroup):
+    number = State()
+    name = State()
 
 async def Help(message : types.message):
     if message.from_user.id == 5221868883:
@@ -35,8 +43,44 @@ async def call_menu(callback : types.CallbackQuery):
     await callback.answer()
 
 async def zak(callback : types.CallbackQuery):
-    await callback.answer('Вы заказали пиццу!', show_alert=True)
-    await bot.send_message(5221868883, f'{callback.from_user.id} заказал {callback.data.split()[1]}')
+    await FSM_client.number.set()
+    global dat, pizza
+    dat = []
+    pizza = ''
+    call = callback.data.split()
+    call.pop(0)
+    print(call)
+    for i in call:
+        pizza += i
+    await bot.send_message(callback.from_user.id, 'Введите ваш номер телефона, который мы можем использовать для связи', reply_markup=kb_onload)
+    await callback.answer()
+
+async def cancel(message : types.message, state : FSMContext):
+    await state.finish()
+    if message.from_user.id == 5221868883:
+        await bot.send_message(message.from_user.id, 'Загрузка отменена!', reply_markup=kb_admin)
+    else:
+        await bot.send_message(message.from_user.id, 'Загрузка отменена!', reply_markup=kb_client)
+
+async def num_zak(message : types.message, state : FSMContext):
+    try:
+        global dat
+        dat.append(int(message.text))
+        await message.reply('Как вас зовут?')
+        await FSM_client.next()
+    except:
+        await message.reply('Введите корректный номер')
+        FSMContext.reset()
+
+async def name_zak(message : types.message, state : FSMContext):
+    global dat, pizza
+    dat.append(message.text)
+    await bot.send_message(5221868883, f'@{message.from_user.username} заказал {pizza}\nТелефон: {dat[0]}\nИмя: {dat[1]}')
+    await state.finish()
+    if message.from_user.id == 5221868883:
+        await bot.send_message(message.from_user.id, f'{dat[1]}, вы успешно заказали пиццу!', reply_markup=kb_admin)
+    else:
+        await bot.send_message(message.from_user.id, f'{dat[1]}, Вы успешно заказали пиццу!', reply_markup=kb_client)
 
 def reg(dp : Dispatcher):
     dp.register_message_handler(Help, commands=['start'])
@@ -48,3 +92,7 @@ def reg(dp : Dispatcher):
     dp.register_message_handler(menu, Text(equals=['меню'], ignore_case=True))
     dp.register_callback_query_handler(call_menu, text='меню')
     dp.register_callback_query_handler(zak, lambda x: x.data and x.data.startswith('заказать '))
+    dp.register_message_handler(cancel, commands=['отмена'], state='*')
+    dp.register_message_handler(cancel, Text(equals=['отмена'], ignore_case=True), state='*')
+    dp.register_message_handler(num_zak, state=FSM_client.number)
+    dp.register_message_handler(name_zak, state=FSM_client.name)
